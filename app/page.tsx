d1b14f7467f79efcf3e6e86ce2afb4bc1a83b4a4
Zizahswiftie <azizahulnha@gmail.com>
2026-01-02 13:33:41 +0700

    portofolio siap publikasi

--- app/page.tsx
+++ app/page.tsx
@@ -4,15 +4,15 @@ export default function Page() {
   return (
     <section>
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        My Portfolio
+        Nur Azizah Ulinnuha
       </h1>
       <p className="mb-4">
-        {`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
-        Vim's keystroke commands and tabs' flexibility for personal viewing
-        preferences. This extends to my support for static typing, where its
-        early error detection ensures cleaner code, and my preference for dark
-        mode, which eases long coding sessions by reducing eye strain.`}
+        {`A creative problem solver specializing in crafting user-centered digital experiences. 
+        I believe that great design is not just about how it looks, but more importantly,
+         about how it works and feels for the human on the other side of the screen.
+         Expertise: Figma, Figjam, Prototyping, and User Research.`}
       </p>
+      <img src="https://i.postimg.cc/HnsxMCDQ/utama.png" alt="Mockup" className="w-full h-auto rounded-xl mt-8" />
       <div className="my-8">
         <BlogPosts />
       </div>
